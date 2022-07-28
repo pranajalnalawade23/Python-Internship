3f6d2057a5031c16d4be32b226b0db84718ff0ec
pranajalnalawade23 <nalawadepranjal2003@gmail.com>
2022-07-28 09:43:00 +0530

    Adding single and multiline comments

--- DAY 3/hello world .py
+++ DAY 3/hello world .py
@@ -1,1 +1,11 @@
+#this is my first program , this is single line comment
 print("hello world");
+'''this is multiline comment 
+this is line 1
+this is line 2 
+this is line 3
+.
+.
+.
+this is line n
+'''
