4c9bb9172b7f669bd3aae3eeaaf30c19f68fbd72
pranajalnalawade23 <nalawadepranjal2003@gmail.com>
2022-08-07 09:18:27 +0530

    operators

---
+++ DAY 6/list.py
@@ -0,0 +1,15 @@
+'''
+Collection of different or same values
+
+
+'''
+
+fruitList = ["grapes", "banana", "apples"]
+print(fruitList)
+
+print(len(fruitList))
+
+numList= [1, 5, 10, 9, 3]
+booleanList = [True, False, False]
+
+mixDatatypeList= ["shiv", 25, True, 50000000, "male"]
